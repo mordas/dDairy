32bdab8c5c1bea5782fb6665584191d80ce8009f
Dmitry2 <mordasdsp@gmail.com>
2018-12-04 12:26:44 +0300

    переделал под use strict

--- js/script.js
+++ js/script.js
@@ -1,47 +1,41 @@
+"use strict"
 let parentElem = document.body;
 let dairy = {};
-// ask = () => {
-//     let note = prompt('Напиши что-нибудь?');
-//         textNote = document.createElement('div');
-//         textNote.className = ('notes');
-//         textNote.innerHTML = note ;
-//         parentElem.appendChild(textNote);
-
-//      let date = new Date();
-//          dateDiv = document.createElement('div');
-//          dateDiv.className = ('date');
-//          dateDiv.innerHTML = `${date.getFullYear()} ${date.getMonth()} ${date.getDay()} ${date.getHours()} часов`;
-//          textNote.appendChild(dateDiv);    
-//     };
-   
-
-
-
+let btnMin = document.getElementById('min'),
+    btnNorm = document.getElementById('norm'),
+    btnMax = document.getElementById('max'),
+    btnFix = document.getElementById('fix');
 
 
     
 
-rndAsk = (num) => {
+let rndAsk = (num) => {
     for (let i = 0; i <= Math.random() * num; i++){
-        ask = () => {
-            let note = prompt('Напиши что-нибудь?');
+      let  ask = () => {
+            let note = prompt('Напиши что-нибудь'),
                 textNote = document.createElement('div');
+                
                 textNote.className = ('notes');
-                textNote.innerHTML = note ;
+                textNote.innerHTML = note;
                 parentElem.appendChild(textNote);
         
-             let date = new Date();
+             let date = new Date(),
                  dateDiv = document.createElement('div');
+                 
                  dateDiv.className = ('date');
                  dateDiv.innerHTML = `${date.getFullYear()} ${date.getMonth()} ${date.getDay()} ${date.getHours()} часов`;
-                 textNote.appendChild(dateDiv); 
+                 textNote.appendChild(dateDiv);
                  dairy[date] = note;     
             };
             ask();
 
     }
-}
+};
 
 
-rndAsk(10);
-console.log(dairy);
+btnMin.addEventListener('click',rndAsk(12));
+
+
+///rndAsk(10);
+///console.log(dairy);
+
